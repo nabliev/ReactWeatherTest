02cc14bbca7e4cc52295db117762b3f198be7e34
Nariman Abliev <ablievn@gmail.com>
2017-07-26 11:06:57 +0300

    changes for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 app.listen(app.get('port'), function() {
-    console.log('Server is up and running on port 3000');
+    console.log('Server is up and running on port', app.get('port'));
 });
