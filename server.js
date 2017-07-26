312815e20cd96a46521e93ebb80c7b5fccfdbfdd
Nariman Abliev <ablievn@gmail.com>
2017-07-26 11:16:59 +0300

    Changes to server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
+app.use(function(req, res, next){
+    if(req.headers['x-forwarded-proto'] === 'http'){
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 app.use(express.static(__dirname + '/public'));
 app.listen(app.get('port'), function() {
     console.log('Server is up and running on port', app.get('port'));
